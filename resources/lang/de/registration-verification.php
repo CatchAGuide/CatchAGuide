d84d0b046f708e8cb9d3021f05b82bda6184e02b
Jimmy <nejimz1101890@gmail.com>
2024-06-27 22:15:43 +0800

    registration email sending

---
+++ resources/lang/de/registration-verification.php
@@ -0,0 +1,14 @@
+<?php 
+return [
+    'subject' => 'Jetzt E-Mail-Adresse bestätigen und Registrierung abschließen!',
+    'hello' => 'Hallo',
+    'loginText' => 'Einloggen',
+    'confirmEmail' => 'bestätige jetzt deine E-Mail-Adresse, um die Registrierung bei Catch A Guide abzuschließen:',
+    'button' => 'Registrierung abschließen',
+    'cantClick' => 'Du kannst den Button nicht klicken? Klicke diesen Link oder kopiere ihn in deinen Browser:',
+    'verifyingEmail' => 'Die Verifizierung deiner Email-Adresse hilft uns dabei, automatische Emails zur Buchung oder Buchungsbestätigung an die korrekte Mailadresse zu verschicken.',
+    'thankYou' => 'Vielen Dank für die Verifizierung deiner Daten.',
+    'regards' => 'Viele Grüße',
+    'cag' => 'Deine Catch A Guide Team',
+    'success_message' => 'Vielen Dank für die Registrierung. Wir haben dir eine automatische Email zur Bestätigung deiner Daten geschickt. Bitte bestätige deine Daten in der Email, um die Registrierung abzuschließen.'
+];
