202f28a9a18825f2f63fb6d40a2a603098bdb407
Lester bon <lesterbonbiono@gmail.com>
2024-12-29 00:06:14 +0800

    Fix on the shore

--- resources/views/pages/category/country.blade.php
+++ resources/views/pages/category/country.blade.php
@@ -548,7 +548,7 @@
                                                             <img src="{{asset('assets/images/icons/fishing-tool-new.svg')}}" height="20" width="20" alt="" />
                                                         <div class="">
                                                             <div class="tours-list__content__trait__text" >
-                                                            {{$guiding->is_boat ? $guiding->boat_type : 'Shore'}}   
+                                                            {{$guiding->is_boat ? ($guiding->boatType && $guiding->boatType->name !== null ? $guiding->boatType->name : __('guidings.boat')) : __('guidings.shore')}}
                                                             </div>
                                                         
                                                         </div>
