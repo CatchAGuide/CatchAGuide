a7ace866320497b7c5a7dd85116beb9d5feecf91
DevLest <lesterbonbiono@gmail.com>
2025-01-24 13:10:18 +0800

    Fixed Lnaguage switch for local and staging

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -11,25 +11,29 @@ class LanguageController extends Controller
         $validatedData = $request->validate([
             'language' => 'required|in:' . implode(',', config('app.locales')),
         ]);
+
+        if (env('APP_ENV') == 'production') {
         
-        $english = ENV('EN_APP_URL', 'https://catchaguide.com');
-        $german = ENV('DE_APP_URL', 'https://catchaguide.de');
-
-        $previousUrl = url()->previous();
-        $previousUrlComponents = parse_url($previousUrl);
-
-        $previousPath = isset($previousUrlComponents['path']) ? $previousUrlComponents['path'] : '';
-
-        if($validatedData['language'] == 'de'){
-            return redirect($german.$previousPath);
+            $english = ENV('EN_APP_URL', 'https://catchaguide.com');
+            $german = ENV('DE_APP_URL', 'https://catchaguide.de');
+    
+            $previousUrl = url()->previous();
+            $previousUrlComponents = parse_url($previousUrl);
+    
+            $previousPath = isset($previousUrlComponents['path']) ? $previousUrlComponents['path'] : '';
+            
+            if($validatedData['language'] == 'de'){
+                return redirect($german.$previousPath);
+            }
+            if($validatedData['language'] == 'en'){
+                return redirect($english.$previousPath);
+            }
         }
-        if($validatedData['language'] == 'en'){
-            return redirect($english.$previousPath);
-        }
-        // app()->setLocale($validatedData['language']);
-        // session()->put('locale', $validatedData['language']);
+
+        app()->setLocale($validatedData['language']);
+        session()->put('locale', $validatedData['language']);
         
-        // return redirect()->back();
+        return redirect()->back();
     }
 
 }
