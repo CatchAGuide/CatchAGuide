be7f7d03bf59d6c8e63d05fdfad279fc9c22297a
DevLest <lesterbonbiono@gmail.com>
2025-06-16 20:25:54 +0800

    updates

--- app/Models/Guiding.php
+++ app/Models/Guiding.php
@@ -519,6 +519,7 @@ class Guiding extends Model
         if ($city || $country) {
             $searchQuery = array_filter([$city, $country, $region], fn($val) => !empty($val));
             $searchString = implode(', ', $searchQuery);
+            Log::info('searchString', ['searchString' => $searchString]);
             
             $translated  = getLocationDetails($searchString);
             if ($translated) {
