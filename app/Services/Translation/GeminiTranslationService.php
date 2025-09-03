ac210a5fea399c00e779e7a43ff6348130800c5b
Lester bon <36285461+DevLest@users.noreply.github.com>
2025-09-04 01:00:17 +0800

    Update for fix on error 500 (#322)

--- app/Services/Translation/GeminiTranslationService.php
+++ app/Services/Translation/GeminiTranslationService.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Client\Response;
+use Illuminate\Http\Request;
 use App\Services\DDoSProtectionService;
 
 class GeminiTranslationService
