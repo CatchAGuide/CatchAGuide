fc5b9ce990a08f9241ed7b4784c41665e9a1ea7d
DevLest <lesterbonbiono@gmail.com>
2026-09-10 18:22:23 +0800

    Fix Safari issue

--- resources/views/pages/home/partials/mobile-bottom-nav.blade.php
+++ resources/views/pages/home/partials/mobile-bottom-nav.blade.php
@@ -19,3 +19,49 @@
         </a>
     @endforeach
 </nav>
+<script>
+(function () {
+    var nav = document.querySelector('.cag-home-bottom-nav');
+    if (!nav || !window.visualViewport || nav.getAttribute('data-cag-vv') === '1') {
+        return;
+    }
+    nav.setAttribute('data-cag-vv', '1');
+
+    var hideNav = window.matchMedia('(min-width: 768px)');
+    var frame = 0;
+
+    function pin() {
+        if (hideNav.matches) {
+            nav.classList.remove('is-vv-pinned');
+            nav.style.removeProperty('--cag-bottom-nav-top');
+            return;
+        }
+        var height = nav.offsetHeight;
+        if (height <= 0) {
+            return;
+        }
+        var vv = window.visualViewport;
+        nav.style.setProperty(
+            '--cag-bottom-nav-top',
+            Math.round(vv.offsetTop + vv.height - height) + 'px'
+        );
+        nav.classList.add('is-vv-pinned');
+    }
+
+    function requestPin() {
+        if (frame) {
+            return;
+        }
+        frame = window.requestAnimationFrame(function () {
+            frame = 0;
+            pin();
+        });
+    }
+
+    pin();
+    window.visualViewport.addEventListener('resize', requestPin);
+    window.visualViewport.addEventListener('scroll', requestPin);
+    window.addEventListener('scroll', requestPin, { passive: true });
+    window.addEventListener('orientationchange', requestPin);
+})();
+</script>
